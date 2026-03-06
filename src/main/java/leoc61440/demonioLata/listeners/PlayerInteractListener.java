efbe41549518e1c8550cb5b3b25955875313d23e
leoc61440-boop <leoc61440@gmail.com>
2026-03-06 16:32:35 -0500

    Implement Listener for PlayerInteractEvent

---
+++ src/main/java/leoc61440/demonioLata/listeners/PlayerInteractListener.java
@@ -0,0 +1,11 @@
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class PlayerInteractListener implements Listener {
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        // Your custom interaction logic here
+    }
+}
